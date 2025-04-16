e5d9bf42b287ec1fe3f60eb92f9da58921050014
lcw <liuchunwei@cctvplus.com>
2025-04-16 11:09:45 +0800

    fix typo of ExampleTest.php add notice for config file test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * cos_config.php is a configuration file for the Laravel COS package.
+ * It is only used for local testing and should not be uploaded to the repository.
+ */
 $currentDir = __DIR__ . '/';
 if(!file_exists($currentDir . 'cos_config.php')) {
     file_put_contents($currentDir . 'cos_config.php', '<?php return [];');
@@ -20,7 +24,7 @@ function getConfig():array
     return $config;
 }
 
-it('test acl succuss', function () {
+it('test acl success', function () {
     \Illuminate\Support\Facades\Config::set('cos.default',getConfig());
     $laravelCos = new \Itinysun\LaravelCos\LaravelCos();
     $acl = $laravelCos->getFileAcl('2025/01/01JH85ZBKZ9VCW0BF2V529P770.jpg');
